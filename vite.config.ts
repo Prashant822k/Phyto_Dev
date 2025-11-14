5e906b9ac792cbd9af83837e7abe1b4fe014c549
chiran46 <149004280+chiran46@users.noreply.github.com>
2025-11-14 14:59:02 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    globals: true,
+    coverage: {
+      provider: 'v8'
+    }
+  }
 }));
